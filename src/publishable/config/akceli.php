01792689d026ee0a82e9b615dd5d235dc402777e
Shawn Pivonka <sprov03@gmail.com>
2019-11-02 20:49:01 -0500

    updated docs in the config file

--- src/publishable/config/akceli.php
+++ src/publishable/config/akceli.php
@@ -45,14 +45,50 @@ return [
     /**
      * Options: 'auto-complete' or 'multiple-choice'
      * will default to 'multiple-choice' if this is missing or set to an invalid option
+     *
+     * This controls how you chose your templates.
+     * (auto-complete): Is useful if you know what templates you have, you can just type the first few letters
+     *      of the template name and when it is selected just press enter.  When you do not know what your options are
+     *      you can simply press enter with nothing selected and it will switch to multiple-choice selection allowing
+     *      for you to see the full lis of templates you want.
      */
     'select-template-behavior' => 'auto-complete',
 
+    /**
+     * This is for documenting what values you want to be show based on a given data type.
+     */
     'column-settings' => [
+        /**
+         * Usage: <?=$column->getColumnSetting('php_class_doc_type', 'string')?>
+         *
+         * Outputs based on column analysis:
+         *    Integer: 'integer'
+         *    String: 'string'
+         *    Enum: 'string'
+         *    Timestamp: 'Carbon'
+         *    Boolean: 'boolean'
+         */
         'php_class_doc_type' => Akceli::columnSetting('string', 'integer', 'string', 'string', 'Carbon', 'boolean'),
+
+        /**
+         * Usage: <?=$column->getColumnSetting('casts', 'string')?>
+         *
+         * Outputs based on column analysis:
+         *    Integer: null
+         *    String: null
+         *    Enum: null
+         *    Timestamp: 'datetime'
+         *    Boolean: 'boolean'
+         */
         'casts' => Akceli::columnSetting(null, null, null, null, 'datetime', 'boolean'),
     ],
 
+    /**
+     * This is where all the magic happens!!
+     *
+     * To make a new command: php artisan akceli new-command
+     * It will register the command in the following list for you and build out the boiler plate of the command class.
+     */
     'generators' => [
         'all' => DefaultAllGenerator::class,
         'new-command' => DefaultNewAkceliGenerator::class,
@@ -80,6 +116,10 @@ return [
         /** New Generators Get Inserted Here */
     ],
 
+    /**
+     * This mapping is used to build relationships
+     * Can be used to add custom relationship and build a Custom RelationshipBuilder
+     */
     'relationships' => [
         'belongsToMany' => BelongsToManyBuilder::class,
         'belongsTo' => BelongsToBuilder::class,
