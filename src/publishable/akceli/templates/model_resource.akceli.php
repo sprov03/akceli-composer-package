403cc1f5440264d7053f357a0edd74cae0a3b5db
Shawn Pivonka <sprov03@gmail.com>
2019-12-14 22:11:14 -0600

    cleaned up the mdoel resource

--- src/publishable/akceli/templates/model_resource.akceli.php
+++ src/publishable/akceli/templates/model_resource.akceli.php
@@ -7,7 +7,6 @@ namespace App\Resources;
 use App\Models\[[ModelName]];
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Broadcasting\Channel;
 
 /**
  * Class [[ModelName]]Resource
@@ -20,8 +19,6 @@ use Illuminate\Broadcasting\Channel;
  */
 class [[ModelName]]Resource extends JsonResource
 {
-    const Model = [[ModelName]]::class;
-
     /**
      * Transform the resource into an array.
      *
@@ -32,7 +29,6 @@ class [[ModelName]]Resource extends JsonResource
     public function toArray($request)
     {
         return [
-            'class_name' => $this->class_name,
 <?php foreach ($table->columns as $column): ?>
 <?php if ($table->columns->last() === $column): ?>
             '<?=$column->Field?>' => $this->resource-><?=$column->Field?>
@@ -41,51 +37,5 @@ class [[ModelName]]Resource extends JsonResource
 <?php endif;?>
 <?php endforeach; ?>
         ];
-    }
-
-    public static function create(
-<?php foreach ($table->columns as $column): ?>
-<?php if ($column->getColumnSetting('php_class_doc_type', null)): ?>
-<?php if($table->columns->last() === $column): ?>
-        <?=$column->getColumnSetting('php_class_doc_type')?> $<?=$column->getField() . "\n"?>
-<?php else: ?>
-        <?=$column->getColumnSetting('php_class_doc_type')?> $<?=$column->getField() . ",\n"?>
-<?php endif; ?>
-<?php else: ?>
-<?php if($table->columns->last() === $column): ?>
-        $<?=$column->Field . "\n"?>
-<?php else: ?>
-        $<?=$column->Field . ",\n"?>
-<?php endif; ?>
-<?php endif; ?>
-<?php endforeach; ?>
-    ) {
-        $[[modelName]] = new [[ModelName]]();
-
-<?php foreach ($table->columns as $column): ?>
-        $[[modelName]]-><?=$column->Field?> = $<?=$column->Field?>;
-<?php endforeach; ?>
-
-        $[[modelName]]->save();
-
-        $[[modelName]]Resource = new [[ModelName]]Resource($[[modelName]]);
-        return $[[modelName]]Resource;
-    }
-
-    /**
-     * @param array $validated
-     * @return $this
-     */
-    public function patch(array $validated)
-    {
-        $this->resource->update($validated);
-    }
-
-
-    /**
-     * @throws \Exception
-     */
-    public function delete() {
-        $this->resource->delete();
     }
 }
