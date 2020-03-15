41fe71abc0aa17a6df97b120ec772ca8060ad856
Shawn Pivonka <shawnpivonka@Shawns-MBP.attlocal.net>
2020-03-15 10:31:37 -0500

    udate the seeder generator

--- src/publishable/akceli/templates/model_seeder.akceli.php
+++ src/publishable/akceli/templates/model_seeder.akceli.php
@@ -4,6 +4,7 @@ use Akceli\TemplateData;?>
 
 use Illuminate\Database\Seeder;
 use App\Models\<?=$table->ModelName?>;
+use Faker\Generator as Faker;
 
 class <?=$table->ModelName?>Seeder extends Seeder
 {
@@ -14,6 +15,9 @@ class <?=$table->ModelName?>Seeder extends Seeder
      */
     public function run()
     {
+        /** @var Faker $faker */
+        $faker = app(Faker::class);
+
         $<?=$table->ModelNames?> = factory(<?=$table->ModelName?>::class, 20)->create([
 <?php foreach ($table->columns as $column): ?>
 <?php if (isset($column->faker_type)): ?>
