8d01f6edb204885e99e23755982a20e6c5b284bf
Shawn Pivonka <shawnpivonka@Shawns-MBP.attlocal.net>
2020-04-30 22:54:05 -0500

    saving for now

--- src/publishable/akceli/bootstrap/sanctum-api/bootstrap.config.php
+++ src/publishable/akceli/bootstrap/sanctum-api/bootstrap.config.php
@@ -25,35 +25,9 @@ return [
             AkceliFileModifier::file('tests/TestCase.php')
                 ->shouldUseTrait('Akceli\RealtimeClientStoreSync\Middleware\ClientStoreTestMiddlewareOverwrites'),
 
-<?php
-
-use Akceli\AkceliFileModifier;
-
-return [
-    'commands' => [
-        // Realtime database
-        'composer require akceli/laravel-realtime-database dev-master',
-        'php artisan vendor:publish --provider="Akceli\RealtimeClientStoreSync\ServiceProvider"',
-
-        // Sanctum
-        'composer require laravel/sanctum',
-        'php artisan vendor:publish --provider="Laravel\Sanctum\SanctumServiceProvider"',
-        'php artisan vendor:publish --tag=sanctum-migrations',
-        'php artisan migrate',
-    ],
-    'string_replacements' => [
-        'App\Models\User' => 'App\Models\User'
-    ],
-    'files_to_remove' => [
-        'app/User.php'
-    ],
-    'file_modifiers' => function () {
-        return [
-            AkceliFileModifier::file('tests/TestCase.php')
-                ->shouldUseTrait('Akceli\RealtimeClientStoreSync\Middleware\ClientStoreTestMiddlewareOverwrites'),
-
             AkceliFileModifier::file('app/Models/User.php')
                 ->shouldUseTrait('Akceli\RealtimeClientStoreSync\ClientStore\ClientStoreModelTrait')
+                ->shouldUseTrait('App\ClientStores\UsersStore')
                 ->addMethodToFile('getStoreProperties', <<<'EOF'
     public function getStoreProperties()
     {
@@ -64,26 +38,6 @@ return [
 EOF
                 ),
 
-            AkceliFileModifier::file('app/Http/Kernel.php')
-                ->addUseStatementToFile('Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful')
-                ->addUseStatementToFile('Akceli\RealtimeClientStoreSync\Middleware\FlushClientStoreChangesMiddleware')
-                ->addLineBelow("'api' => [", '            EnsureFrontendRequestsAreStateful::class,')
-                ->addLineBelow('protected $routeMiddleware = [', '        \'client-store\' => FlushClientStoreChangesMiddleware::class,'),
-
-            AkceliFileModifier::file('app/Providers/AppServiceProvider.php')
-                ->addToTopOfMethod('register', 'Sanctum::ignoreMigrations();')
-                ->addUseStatementToFile('Laravel\Sanctum\Sanctum'),
-
-            AkceliFileModifier::file('.env.example')
-                ->addLineBelow('APP_URL', 'MIX_CLIENT_STORE_URL="${CLIENT_STORE_URL}"')
-                ->addLineBelow('APP_URL', 'CLIENT_STORE_URL=api/client-store'),
-
-            AkceliFileModifier::file('.env')
-                ->addLineBelow('APP_URL', 'MIX_CLIENT_STORE_URL="${CLIENT_STORE_URL}"')
-                ->addLineBelow('APP_URL', 'CLIENT_STORE_URL=api/client-store'),
-        ];
-    },
-];
             AkceliFileModifier::file('app/Http/Kernel.php')
                 ->addUseStatementToFile('Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful')
                 ->addUseStatementToFile('Akceli\RealtimeClientStoreSync\Middleware\FlushClientStoreChangesMiddleware')
